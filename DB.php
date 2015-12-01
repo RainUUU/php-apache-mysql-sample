cec1c01dfb2f885673843a7e31d8b1ad84bd7f17
Rain Tao <425825703@qq.com>
2015-12-01 17:13:50 +0800

    Update DB.php

--- DB.php
+++ DB.php
@@ -8,10 +8,10 @@ class DB
 
     function __construct()
     {
-        $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
-        $databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
-        $username = env("MYSQL_USERNAME", "homestead");
-        $password = env("MYSQL_PASSWORD", "secret");
+        $serverName = env("MYSQL_PORT_3306_TCP_ADDR", "10.10.26.58");
+        $databaseName = env("MYSQL_INSTANCE_NAME", "tHgIWMfp9NCJ6xi4");
+        $username = env("MYSQL_USERNAME", "u1tMlIoiSNDshgRB");
+        $password = env("MYSQL_PASSWORD", "p9crgd4s2mXiahzIb");
 
         try {
             $this->pdo = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
